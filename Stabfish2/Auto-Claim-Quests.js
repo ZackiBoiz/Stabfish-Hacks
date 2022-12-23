88ffc680549e6e3c50342e63a249de38aa423d53
Zacki <95724562+ZackiBoiz@users.noreply.github.com>
2022-12-23 18:08:04 -0500

    Create Auto-Claim-Quests.js

---
+++ Stabfish2/Auto-Claim-Quests.js
@@ -0,0 +1,10 @@
+setInterval(function() {
+  if (document.querySelector('div.align-items-center.d-flex > .btn-sm.btn-orange.btn')) {
+    try {
+      document.querySelector('div.align-items-center.d-flex > .btn-sm.btn-orange.btn').click();
+      setTimeout(function() {
+        document.querySelector('.mt-2.justify-content-center.d-flex > .btn-sm.btn-orange.btn').click();
+      }, 250);
+    catch (e) {}
+  }
+});
