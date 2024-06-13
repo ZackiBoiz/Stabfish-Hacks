9fe418cb123d843c956ad529e32b33fb0fc2f9b2
Zacki <95724562+ZackiBoiz@users.noreply.github.com>
2024-06-13 13:09:33 -0400

    Delete Stabfish/Unlock-All-Masters.js

--- Stabfish/Unlock-All-Masters.js
+++
@@ -1,2 +0,0 @@
-localStorage.setItem("unlockedMasters","[true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true]");
-window.location.reload();
