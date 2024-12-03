3e4c96b2076ce82d92f5e73525b4d351840e0a59
Zacki <95724562+ZackiBoiz@users.noreply.github.com>
2024-12-03 15:33:27 -0500

    Update Unlock-All-Stats.js
    Update to Stabfish.io v29, Add max spears

--- Stabfish/Unlock-All-Stats.js
+++ Stabfish/Unlock-All-Stats.js
@@ -1,12 +1,16 @@
-var unlocks = {
-  "unlockedHats": 37,
-  "unlockedMasters": 58,
-  "unlockedFaces": 45,
-  "unlockedBodies": 58
+const unlocks = {
+  "unlockedHats": 39,
+  "unlockedMasters": 61,
+  "unlockedFaces": 46,
+  "unlockedBodies": 61
 };
+const spears = 6;
+const spear_levels = 3;
+
 for (var [unlock, amt] of Object.entries(unlocks)) {
   var val = JSON.parse(localStorage[unlock]);
   var filled = new Array(amt + 1).fill(true);
   localStorage[unlock] = JSON.stringify(filled);
 }
+localStorage.unlockedSpears = JSON.stringify(new Array(spears).fill(spear_levels));
 window.location.reload();
