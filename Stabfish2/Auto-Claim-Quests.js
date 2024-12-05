76be95089d0def34b72d27aa5fc4d9e3469f24f9
Zacki <95724562+ZackiBoiz@users.noreply.github.com>
2024-12-05 07:47:46 -0500

    Fix syntax error

--- Stabfish2/Auto-Claim-Quests.js
+++ Stabfish2/Auto-Claim-Quests.js
@@ -5,6 +5,6 @@ setInterval(function() {
       setTimeout(function() {
         document.querySelector('.mt-2.justify-content-center.d-flex > .btn-sm.btn-orange.btn').click();
       }, 250);
-    catch (e) {}
+    } catch (e) {}
   }
 });
